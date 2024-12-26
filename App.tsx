873cee7e7fe54961097e14a17623cecefe7d5565
Bognar Junior <bognar_junior@yahoo.com.br>
2024-12-26 16:54:09 -0300

    Install Modules
    Install new modules and fonts

--- App.tsx
+++ App.tsx
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  useFonts, 
+  Ubuntu_700Bold, 
+  Ubuntu_500Medium, 
+  Ubuntu_400Regular 
+} from '@expo-google-fonts/ubuntu';
+import Home from '@/app/home';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Ubuntu_700Bold, 
+    Ubuntu_500Medium, 
+    Ubuntu_400Regular
+  })
+
+  if(!fontsLoaded){
+    return;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+   <Home />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
